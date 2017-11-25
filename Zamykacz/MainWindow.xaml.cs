e4292e0d94b31357078d7a0a8700f05bd46cb7a3
Karol Suchecki <kajoj7@gmail.com>
2017-11-25 18:36:58 +0100

    Application

---
+++ Zamykacz/MainWindow.xaml.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace Zamykacz
+{
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      private int _shortcutHour = 0;
+      private DispatcherTimer _timer;
+      private TimeSpan _time;
+      public MainWindow()
+      {
+         InitializeComponent();
+      }
+
+   
+
+      private void Cancel_Click(object sender, RoutedEventArgs e)
+      {
+         _timer.Stop();
+         Window.Background = Brushes.White;
+         StartGroupBox.Visibility = Visibility.Visible;
+         CancelGroupBox.Visibility = Visibility.Hidden;
+         EnableAllControls();
+      }
+
+      private void Start_Click(object sender, RoutedEventArgs e)
+      {
+         if (CloseAtTimePicker.Value == null && CloseForTimePicker.Value == null && !(bool)OneHourShortcut.IsChecked && !(bool)TwoHourShortcut.IsChecked && !(bool)ThreeHourShortcut.IsChecked)
+         {
+            MessageBox.Show("Nie ustawiono za ile lub o której godzinie ma zostać wyłączony system.", "Ups...");
+            return;
+         }
+         if (CloseAtTimePicker.Value != null)
+         {
+            if (DateTime.Now >= CloseAtTimePicker.Value.Value)
+            {
+               MessageBox.Show("Czas kiedy system ma zostać zamknięty musi być czasem przyszłym!", "Ups...");
+               return;
+            }
+            CloseAtValueLabel.Content = CloseAtTimePicker.Value.Value.ToString("HH:mm:ss dd-MM-yyyy");
+            TimeSpan timeSpan = CloseAtTimePicker.Value.Value.Subtract(DateTime.Now);
+            CloseForValueLabel.Content = _time = new TimeSpan(timeSpan.Days, timeSpan.Hours, timeSpan.Minutes,
+               timeSpan.Seconds);
+         }
+         else if (CloseForTimePicker.Value != null)
+         {
+            var timeToClose = DateTime.Now.Add(new TimeSpan(0,CloseForTimePicker.Value.Value.Hour, CloseForTimePicker.Value.Value.Minute, CloseForTimePicker.Value.Value.Second));
+            CloseAtValueLabel.Content = timeToClose.ToString("HH:mm:ss dd-MM-yyyy");
+            CloseForValueLabel.Content = _time = new TimeSpan(0, CloseForTimePicker.Value.Value.Hour, CloseForTimePicker.Value.Value.Minute, CloseForTimePicker.Value.Value.Second);
+         }
+         else if (_shortcutHour > 0)
+         {
+            var timeToClose = DateTime.Now.AddHours(_shortcutHour);
+            CloseAtValueLabel.Content = timeToClose.ToString("HH:mm:ss dd-MM-yyyy");
+            CloseForValueLabel.Content = _time = new TimeSpan(0, _shortcutHour, 0,0);
+         }
+         PrepareLook();
+         RunTimer();
+      }
+
+      private void OneHourShortcut_Click(object sender, RoutedEventArgs e)
+      {
+         _shortcutHour = 1;
+         ResetTimePickers();
+         TwoHourShortcut.IsChecked = false;
+         ThreeHourShortcut.IsChecked = false;
+      }
+
+      private void TwoHourShortcut_Click(object sender, RoutedEventArgs e)
+      {
+         _shortcutHour = 2;
+         ResetTimePickers();
+         OneHourShortcut.IsChecked = false;
+         ThreeHourShortcut.IsChecked = false;
+      }
+
+      private void ThreeHourShortcut_Click(object sender, RoutedEventArgs e)
+      {
+         _shortcutHour = 3;
+         ResetTimePickers();
+         OneHourShortcut.IsChecked = false;
+         TwoHourShortcut.IsChecked = false;
+      }
+
+      private void CloseForTimePicker_LostFocus(object sender, RoutedEventArgs e)
+      {
+         if (CloseForTimePicker.Value != null)
+            CloseAtTimePicker.Value = null;
+      }
+
+      private void CloseAtTimePicker_LostFocus(object sender, RoutedEventArgs e)
+      {
+         if (CloseAtTimePicker.Value != null)
+            CloseForTimePicker.Value = null;
+      }
+
+
+      private void CloseForTimePicker_GotFocus(object sender, RoutedEventArgs e)
+      {
+         UncheckedToggleButtons();
+      }
+
+      private void CloseAtTimePicker_GotFocus(object sender, RoutedEventArgs e)
+      {
+         UncheckedToggleButtons();
+      }
+
+      private void PrepareLook()
+      {
+         Window.Background = new SolidColorBrush(Color.FromRgb(247, 186, 186));
+         DisableAllControls();
+         UncheckedToggleButtons();
+         StartGroupBox.Visibility = Visibility.Hidden;
+         CancelGroupBox.Visibility = Visibility.Visible;
+      }
+
+      private void DisableAllControls()
+      {
+         OneHourShortcut.IsEnabled = false;
+         TwoHourShortcut.IsEnabled = false;
+         ThreeHourShortcut.IsEnabled = false;
+         CloseAtTimePicker.IsEnabled = false;
+         CloseForTimePicker.IsEnabled = false;
+      }
+
+      private void EnableAllControls()
+      {
+         OneHourShortcut.IsEnabled = true;
+         TwoHourShortcut.IsEnabled = true;
+         ThreeHourShortcut.IsEnabled = true;
+         CloseAtTimePicker.IsEnabled = true;
+         CloseForTimePicker.IsEnabled = true;
+      }
+
+      private void UncheckedToggleButtons()
+      {
+         _shortcutHour = 0;
+         OneHourShortcut.IsChecked = false;
+         TwoHourShortcut.IsChecked = false;
+         ThreeHourShortcut.IsChecked = false;
+      }
+
+      private void ResetTimePickers()
+      {
+         CloseForTimePicker.Value = null;
+         CloseAtTimePicker.Value = null;
+      }
+
+      private void RunTimer()
+      {
+         _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+         {
+            if (_time == TimeSpan.Zero) _timer.Stop();
+            _time = _time.Add(TimeSpan.FromSeconds(-1));
+            CloseForValueLabel.Content = _time;
+         }, Application.Current.Dispatcher);
+
+         _timer.Start();
+      }
+   }
+}
